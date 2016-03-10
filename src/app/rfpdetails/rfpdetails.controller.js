bda4da5e5318e7d1c84c1be6471e12e3d12d02f4
yoginimokashi23 <mokashi.yogini@mahindra.com>
2016-03-10 18:00:23 +0530

    Added navbar

--- src/app/rfpdetails/rfpdetails.controller.js
+++ src/app/rfpdetails/rfpdetails.controller.js
@@ -1,45 +1,45 @@
 export class RFPDetailsController {
-  constructor () {
+  constructor() {
     'ngInject';
-    
-    
+
+
     this.routes = [{
-    	fromCity : 'Kalwa', 
-    	fromState : 'Maharashtra', 
-    	toCity : 'Delhi', 
-    	toState : 'Kalwa',
-    	oneWay : 'One way',
-    	serviceType : 'LTL',
-    	vehicleType : '16Mt Tauras',
-    	transitTime : '12 Days',
-    	productDescription : 'Lorem',
-    	packagingType : 'Corrugated Boxes',
-    	stack : 'Lon'
-    },
-    {fromCity : 'Kalwa', 
-    	fromState : 'Maharashtra', 
-    	toCity : 'Delhi', 
-    	toState : 'Kalwa',
-    	oneWay : 'One way',
-    	serviceType : 'LTL',
-    	vehicleType : '16Mt Tauras',
-    	transitTime : '12 Days',
-    	productDescription : 'Lorem',
-    	packagingType : 'Corrugated Boxes',
-    	stack : 'Lon'},
-    	{
-    		fromCity : 'Kalwa', 
-    	fromState : 'Maharashtra', 
-    	toCity : 'Delhi', 
-    	toState : 'Kalwa',
-    	oneWay : 'One way',
-    	serviceType : 'LTL',
-    	vehicleType : '16Mt Tauras',
-    	transitTime : '12 Days',
-    	productDescription : 'Lorem',
-    	packagingType : 'Corrugated Boxes',
-    	stack : 'Lon'
-    	}]
+      fromCity: 'Kalwa',
+      fromState: 'Maharashtra',
+      toCity: 'Delhi',
+      toState: 'Kalwa',
+      oneWay: 'One way',
+      serviceType: 'LTL',
+      vehicleType: '16Mt Tauras',
+      transitTime: '12 Days',
+      productDescription: 'Lorem',
+      packagingType: 'Corrugated Boxes',
+      stack: 'Lon'
+    }, {
+      fromCity: 'Kalwa',
+      fromState: 'Maharashtra',
+      toCity: 'Delhi',
+      toState: 'Kalwa',
+      oneWay: 'One way',
+      serviceType: 'LTL',
+      vehicleType: '16Mt Tauras',
+      transitTime: '12 Days',
+      productDescription: 'Lorem',
+      packagingType: 'Corrugated Boxes',
+      stack: 'Lon'
+    }, {
+      fromCity: 'Kalwa',
+      fromState: 'Maharashtra',
+      toCity: 'Delhi',
+      toState: 'Kalwa',
+      oneWay: 'One way',
+      serviceType: 'LTL',
+      vehicleType: '16Mt Tauras',
+      transitTime: '12 Days',
+      productDescription: 'Lorem',
+      packagingType: 'Corrugated Boxes',
+      stack: 'Lon'
+    }]
   }
 
 }
