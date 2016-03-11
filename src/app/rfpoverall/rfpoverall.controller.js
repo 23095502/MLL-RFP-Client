7c6eef5f83484273475a97fb26da644a65afa684
praveensinghhart <23148127@mahindra.com>
2016-03-11 16:00:59 +0530

    fetched data for RFP, aded required fields

--- src/app/rfpoverall/rfpoverall.controller.js
+++ src/app/rfpoverall/rfpoverall.controller.js
@@ -1,44 +1,123 @@
 export class RFPOverallController {
-  constructor() {
+  constructor($http) {
     'ngInject';
-this.AGEOFTRUCK_option=[{name:"5", val:"5"},{name:"5", val:"6"}]
-this.ISMULTIDROP_option=[{name:"N", val:"N"},{name:"Y", val:"Y"}]
-this.ISHUBORWHREQ_option=[{name:"6", val:"6"},{name:"7", val:"7"}]
-this.RATEUOM_option=[{name:"9", val:"9"},{name:"19", val:"19"}]
-this.CARGOTYPE_option=[{name:"23", val:"23"},{name:"29", val:"29"}]
+
+    $http.get('http://59.160.18.222/RFPRest/RFPRestService.svc/GetCustomers?customerid=0&customercode=&customername=&address=&email=&contactperson=&contactno=&cashaccountid=&totalspend=0&active=A&createdby=1&createdon=2016-03-01&mode=GETALL')
+      .then((res) => {
+        this.CUSTOMERNAME_option = res.data;
+      }, (err) => {
+        console.error(err);
+      });
+
+      this.$http = $http;
+
+
+
+    this.CUSTOMERNAME_option = [];
+
+
+    this.customer = {
+      "ACTIVE": null,
+      "ADDRESS": null,
+      "CASHACCOUNTID": null,
+      "CONTACTNO": null,
+      "CONTACTPERSON": null,
+      "CUSTOMERCODE": null,
+      "CUSTOMERID": null,
+      "CUSTOMERNAME": null,
+      "EMAIL": null,
+      "TOTALSPEND": 0.00
+    };
+
+    this.AGEOFTRUCK_option = [{
+      name: "5",
+      val: "5"
+    }, {
+      name: "5",
+      val: "6"
+    }];
+
+    this.ISMULTIDROP_option = [{
+      name: "N",
+      val: "N"
+    }, {
+      name: "Y",
+      val: "Y"
+    }];
+
+    this.ISHUBORWHREQ_option = [{
+      name: "6",
+      val: "6"
+    }, {
+      name: "7",
+      val: "7"
+    }];
+
+    this.RATEUOM_option = [{
+      name: "9",
+      val: "9"
+    }, {
+      name: "19",
+      val: "19"
+    }];
+
+    this.CARGOTYPE_option = [{
+      name: "23",
+      val: "23"
+    }, {
+      name: "29",
+      val: "29"
+    }];
+
     this.overall = {
-      "RFPID": 1,
-      "RFPCODE": "1",
-      "RFPDATE": "2016-01-01 00:00:00",
-      "CUSTOMERID": 5,
-      "INDUSTRYTYPEID": 1,
-      "RFPAMOUNT": 1000,
-      "STARTDATE": "2016-01-01 00:00:00",
-      "RFPOWNER": 1,
-      "CURRENTSTAGINGOWNER": 1,
-      "DIESELRATE": 10,
-      "AGEOFTRUCK": 1,
-      "RFPDESC": "1",
-      "DUEDATE": "2016-01-01 00:00:00",
-      "PRODUCTDESC": "6",
-      "CASHOPPID": "6",
-      "OPPRDOMAIN": "6",
-      "DISTRIBUTIONTYPE": "6",
-      "ISMULTIDROP": "N",
-      "ISHUBORWHREQ": "6",
-      "CARGOTYPE": "6",
-      "SEARCH1": "6",
-      "SEARCH2": "6",
-      "SEARCH3": "6",
-      "PAYMENTTERM": 1000,
-      "RATEUOM": "6",
-      "PENALITIES": "6",
-      "DETENTION": "6",
-      "ESCCLAUSE": "6",
-      "ACTIVE": "A",
-      "CREATEDBY": "1",
-      "CREATEDON": "2016-01-01 00:00:00"
-    }
+      "RFPID": null,
+      "RFPCODE": null,
+      "RFPDATE": null,
+      "CUSTOMERID": null,
+      "INDUSTRYTYPEID": null,
+      "RFPAMOUNT": null,
+      "STARTDATE": null,
+      "RFPOWNER": null,
+      "CURRENTSTAGINGOWNER": null,
+      "DIESELRATE": null,
+      "AGEOFTRUCK": null,
+      "RFPDESC": null,
+      "DUEDATE": null,
+      "PRODUCTDESC": null,
+      "CASHOPPID": null,
+      "OPPRDOMAIN": null,
+      "DISTRIBUTIONTYPE": null,
+      "ISMULTIDROP": null,
+      "ISHUBORWHREQ": null,
+      "CARGOTYPE": null,
+      "SEARCH1": null,
+      "SEARCH2": null,
+      "SEARCH3": null,
+      "PAYMENTTERM": null,
+      "RATEUOM": null,
+      "PENALITIES": null,
+      "DETENTION": null,
+      "ESCCLAUSE": null,
+      "ACTIVE": null,
+      "CREATEDBY": null,
+      "CREATEDON": null
+    };
 
   }
+
+  getRFP() {
+    console.log(this.customer.CUSTOMERID)
+
+    this.$http.get(`http://59.160.18.222/RFPRest/RFPRestService.svc/getrfpbycustomerid/${this.customer.CUSTOMERID}`)
+      .then((res) => {
+        console.log(res);
+        this.overall = res.data[0];
+      }, (err) => {
+        console.error(err);
+      });
+
+
+  }
+
+
 }
