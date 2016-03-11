276c16b666b4f774f616917df08585389853d67e
praveensinghhart <23148127@mahindra.com>
2016-03-11 10:28:34 +0530

    Dropdown is created

--- src/app/rfpoverall/rfpoverall.controller.js
+++ src/app/rfpoverall/rfpoverall.controller.js
@@ -1,7 +1,11 @@
 export class RFPOverallController {
   constructor() {
     'ngInject';
-
+this.AGEOFTRUCK_option=[{name:"5", val:"5"},{name:"5", val:"6"}]
+this.ISMULTIDROP_option=[{name:"N", val:"N"},{name:"Y", val:"Y"}]
+this.ISHUBORWHREQ_option=[{name:"6", val:"6"},{name:"7", val:"7"}]
+this.RATEUOM_option=[{name:"9", val:"9"},{name:"19", val:"19"}]
+this.CARGOTYPE_option=[{name:"23", val:"23"},{name:"29", val:"29"}]
     this.overall = {
       "RFPID": 1,
       "RFPCODE": "1",
