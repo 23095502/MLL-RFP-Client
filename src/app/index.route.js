3113ae069ac01c1bd43cf7d39c7800797d190375
Satyajeet K. Thakare <satyajeetthakre@gmail.com>
2016-03-11 14:02:54 +0530

    Created modal popup UI

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -7,16 +7,19 @@ export function routerConfig($stateProvider, $urlRouterProvider) {
       controller: 'MainController',
       controllerAs: 'main'
     })
+
     .state('rfpoverall', {
       url: '/rfpoverall',
       templateUrl: 'app/rfpoverall/rfpoverall.html',
       controller: 'RFPOverallController',
       controllerAs: 'rfpdetails'
-    }).state('rfpdetails', {
+    })
+
+    .state('rfpdetails', {
       url: '/rfpdetails',
       templateUrl: 'app/rfpdetails/rfpdetails.html',
       controller: 'RFPDetailsController',
-      controllerAs: 'rfp'
+      controllerAs: 'rfplanes'
     });
 
   $urlRouterProvider.otherwise('/');
