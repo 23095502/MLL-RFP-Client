819cc15ef7a6aeb81bb14b76519b52872c8e5294
BHABAL NIKESH <23145258@mahindra.com>
2016-03-14 22:30:51 +0530

    *Add new customer from "Overall" page *Once added customer, refresh customer list in dropdown *Save RFP Overall input details *Once RFP Overall page saved, redirect to the RFP Detail page with RFP ID

--- src/app/rfpoverall/rfpoverall.controller.js
+++ src/app/rfpoverall/rfpoverall.controller.js
@@ -1,48 +1,67 @@
 export class RFPOverallController {
-  constructor($http) {
+  constructor($http, $state) {
     'ngInject';
 
-    $http.get('http://59.160.18.222/RFPRest/RFPRestService.svc/GetCustomers?customerid=0&customercode=&customername=&address=&email=&contactperson=&contactno=&cashaccountid=&totalspend=0&active=A&createdby=1&createdon=2016-03-01&mode=GETALL')
-      .then((res) => {
-        this.CUSTOMERNAME_option = res.data;
-      }, (err) => {
-        console.error(err);
-      });
-
     this.$http = $http;
-
-
-
+    this.$state = $state;
+    //-------------
+    //Get All Cutomers
+    this.getAllCustomers();
+    //-------------
     this.CUSTOMERNAME_option = [];
 
-
     this.customer = {
-      "ACTIVE": null,
-      "ADDRESS": null,
-      "CASHACCOUNTID": null,
-      "CONTACTNO": null,
-      "CONTACTPERSON": null,
+      "CUSTOMERID": 0,
       "CUSTOMERCODE": null,
-      "CUSTOMERID": null,
       "CUSTOMERNAME": null,
+      "ADDRESS": null,
+      "CONTACTPERSON": null,
+      "CONTACTNO": null,
+      "CASHACCOUNTID": null,
+      "TOTALSPEND": null,
       "EMAIL": null,
-      "TOTALSPEND": 0.00
+      "ACTIVE": "A",
+      "MODE": "INSERT",
+      "CREATEDBY": 0,
+      "CREATEDON": null
     };
+
+    this.newCustomer = {
+      "CUSTOMERID": 0,
+      "CUSTOMERCODE": null,
+      "CUSTOMERNAME": null,
+      "ADDRESS": null,
+      "CONTACTPERSON": null,
+      "CONTACTNO": null,
+      "CASHACCOUNTID": null,
+      "TOTALSPEND": null,
+      "EMAIL": null,
+      "ACTIVE": "A",
+      "MODE": "INSERT",
+      "CREATEDBY": 0,
+      "CREATEDON": null
+    };
+
+
 
     this.AGEOFTRUCK_option = _.times(10, (i) => ({
       name: i,
       val: i
     }));
 
-    this.ISMULTIDROP_option = _.map(['Primary', 'Secondary', 'Distribution'], (i) => ({
+    this.DISTRIBUTIONTYPE_option = _.map(['Primary', 'Secondary', 'Distribution'], (i) => ({
       name: i,
       val: i
     }));
 
-    this.ISHUBORWHREQ_option = _.map(['Yes', 'No'], (i) => ({
+    /*this.ISHUBORWHREQ_option = _.map(['Yes', 'No'], (i) => ({
       name: i,
       val: i
-    }));
+    }));*/
+
+    this.ISMULTIDROP_option = [{name:'Yes', val:'Y'},{name:'No', val:'N'}]
+
+    this.ISHUBORWHREQ_option = [{name:'Yes', val:'Y'},{name:'No', val:'N'}]
 
     this.RATEUOM_option = _.map(['PTPK', 'Per trip', 'Per Kg', 'Per Km', 'Per month'], (i) => ({
       name: i,
@@ -57,44 +76,61 @@ export class RFPOverallController {
 
 
     this.overall = {
-      "RFPID": null,
-      "RFPCODE": null,
-      "RFPDATE": null,
-      "CUSTOMERID": null,
-      "INDUSTRYTYPEID": null,
-      "RFPAMOUNT": null,
-      "STARTDATE": null,
-      "RFPOWNER": null,
-      "CURRENTSTAGINGOWNER": null,
-      "DIESELRATE": null,
-      "AGEOFTRUCK": null,
-      "RFPDESC": null,
-      "DUEDATE": null,
-      "PRODUCTDESC": null,
-      "CASHOPPID": null,
-      "OPPRDOMAIN": null,
-      "DISTRIBUTIONTYPE": null,
-      "ISMULTIDROP": null,
+      "RFPID": 0,
+      "RFPCODE": '',
+      "RFPDATE": '1800-01-01 00:00:00',
+      "CUSTOMERID": 0,
+      "INDUSTRYTYPEID": 1,
+      "RFPAMOUNT": 0,
+      "STARTDATE": '1800-01-01 00:00:00',
+      "RFPOWNER": 1,
+      "CURRENTSTAGINGOWNER": 1,
+      "DIESELRATE": 0,
+      "AGEOFTRUCK": 0,
+      "RFPDESC": '',
+      "DUEDATE": '1800-01-01 00:00:00',
+      "PRODUCTDESC": '',
+      "CASHOPPID": '',
+      "OPPRDOMAIN": '',
+      "DISTRIBUTIONTYPE": '',
+      "ISMULTIDROP": this.ISMULTIDROP_option[1].val,
       "ISHUBORWHREQ": this.ISHUBORWHREQ_option[1].val,
-      "CARGOTYPE": null,
-      "SEARCH1": null,
-      "SEARCH2": null,
-      "SEARCH3": null,
-      "PAYMENTTERM": null,
-      "RATEUOM": null,
-      "PENALITIES": null,
-      "DETENTION": null,
-      "ESCCLAUSE": null,
-      "ACTIVE": null,
-      "CREATEDBY": null,
-      "CREATEDON": null
+      "CARGOTYPE": '',
+      "SEARCH1": '',
+      "SEARCH2": '',
+      "SEARCH3": '',
+      "PAYMENTTERM": 0,
+      "RATEUOM": '',
+      "PENALITIES": '',
+      "DETENTION": '',
+      "ESCCLAUSE": '',
+      "ACTIVE": 'A',
+      "CREATEDBY": 5,
+      "CREATEDON": '1800-01-01 00:00:00',
+      "ADDRESS": '',
+      "CONTACTPERSON": '',
+      "CONTACTNO": '',
+      "CASHACCOUNTID": '',
+      "TOTALSPEND": '',
+      "DISTANCE": 0
     };
 
   }
 
+  getAllCustomers() {
+    this.$http.get('http://59.160.18.222/RFPRest/RFPRestService.svc/customer/0')
+      .then((res) => {
+        console.log(res.data);
+
+        this.CUSTOMERNAME_option = res.data;
+      }, (err) => {
+        console.error(err);
+      });
+  }
+
   getRFP() {
 
-    this.$http.get(`http://59.160.18.222/RFPRest/RFPRestService.svc/getrfpbycustomerid/${this.customer.CUSTOMERID}`)
+    /*this.$http.get(`http://59.160.18.222/RFPRest/RFPRestService.svc/getrfpbycustomerid/${this.customer.CUSTOMERID}`)
       .then((res) => {
         console.log(res);
         this.overall = res.data.getrfpbycustomeridResult[0];
@@ -102,14 +138,55 @@ export class RFPOverallController {
         this.overall.DUEDATE = new Date(this.overall.DUEDATE);
       }, (err) => {
         console.error(err);
-      });
+      });*/
   }
 
-  addRfpHeader() {
-    console.log("Hello");
+  addCustomer() {
+
     var req = {
       method: 'POST',
-      url: 'http://59.160.18.222/RFPRest/RFPRestService.svc/rfp/0/INSERT',
+      url: 'http://59.160.18.222/RFPRest/RFPRestService.svc/updtcustomer',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: {
+        "cust": {
+          "CUSTOMERID": this.newCustomer.CUSTOMERID,
+          "CUSTOMERCODE": this.newCustomer.CUSTOMERCODE,
+          "CUSTOMERNAME": this.newCustomer.CUSTOMERNAME,
+          "ADDRESS": this.newCustomer.ADDRESS,
+          "CONTACTPERSON": this.newCustomer.CONTACTPERSON,
+          "CONTACTNO": this.newCustomer.CONTACTNO,
+          "CASHACCOUNTID": this.newCustomer.CASHACCOUNTID,
+          "TOTALSPEND": this.newCustomer.TOTALSPEND,
+          "EMAIL": this.newCustomer.EMAIL,
+          "ACTIVE": "A",
+          "MODE": "INSERT",
+          "CREATEDBY": 5,
+          "CREATEDON": "2016-01-01 00:00:00"
+        }
+      }
+
+    };
+
+    this.$http(req).then((response) => {
+      console.log(response)
+        //-------------
+        //Get All Cutomers
+        this.getAllCustomers();
+        //-------------
+    }, (response) => {
+      console.log(error)
+    });
+
+  }
+
+
+  addRfpHeader() {
+    //console.log("Hello");
+    var req = {
+      method: 'POST',
+      url: 'http://59.160.18.222/RFPRest/RFPRestService.svc/rfp/INSERT',
       headers: {
         'Content-Type': 'application/json'
       },
@@ -117,7 +194,7 @@ export class RFPOverallController {
         "rfpupdt": {
           "RFPID": 0,
           "RFPCODE": this.overall.RFPCODE,
-          "RFPDATE": "2016-01-01 00:00:00",
+          "RFPDATE": "2016-01-01 00:00:00", //this.overall.RFPDATE,
           "CUSTOMERID": this.customer.CUSTOMERID,
           "INDUSTRYTYPEID": this.overall.INDUSTRYTYPEID,
           "RFPAMOUNT": this.overall.RFPAMOUNT,
@@ -127,13 +204,13 @@ export class RFPOverallController {
           "DIESELRATE": this.overall.DIESELRATE,
           "AGEOFTRUCK": this.overall.AGEOFTRUCK,
           "RFPDESC": this.overall.RFPDESC,
-          "DUEDATE": "2016-01-01 00:00:00",
+          "DUEDATE": "2016-01-01 00:00:00", //this.overall.DUEDATE,
           "PRODUCTDESC": this.overall.PRODUCTDESC,
           "CASHOPPID": this.overall.CASHOPPID,
           "OPPRDOMAIN": this.overall.OPPRDOMAIN,
           "DISTRIBUTIONTYPE": this.overall.DISTRIBUTIONTYPE,
           "ISMULTIDROP": this.overall.ISMULTIDROP,
-          "ISHUBORWHREQ": 'N',
+          "ISHUBORWHREQ": this.overall.ISHUBORWHREQ,
           "CARGOTYPE": this.overall.CARGOTYPE,
           "SEARCH1": "6",
           "SEARCH2": "6",
@@ -145,18 +222,26 @@ export class RFPOverallController {
           "ESCCLAUSE": "6",
           "ACTIVE": "A",
           "CREATEDBY": "1",
-          "CREATEDON": "2016-01-01 00:00:00"
+          "CREATEDON": "2016-01-01 00:00:00",
+          "ADDRESS": this.customer.ADDRESS,
+          "CONTACTPERSON": this.customer.CONTACTPERSON,
+          "CONTACTNO": this.customer.CONTACTNO,
+          "CASHACCOUNTID": this.customer.CASHACCOUNTID,
+          "TOTALSPEND": this.customer.TOTALSPEND
         }
+      }
+
     }
 
+    this.$http(req).then((response) => {
+      console.log(response.data.rfpResult[0].RFPID);
+
+      this.$state.go('rfpdetails',{rfpId: response.data.rfpResult[0].RFPID});
+
+    }, (error) => {
+      console.log(error)
+    });
+
   }
-
-  this.$http(req).then(function(response) {
-    console.log(response)
-  }, function(response) {
-    console.log(error)
-  });
-
-}
 
 }
