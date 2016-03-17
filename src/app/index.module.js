66f9c2446a4e3847289937ad327227d41634e2bc
Satyajeet K. Thakare <satyajeetthakre@gmail.com>
2016-03-17 17:44:21 +0530

    Removed unused dependencies

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,10 +3,6 @@
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
-import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
-import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
-import { NavbarDirective } from '../app/components/navbar/navbar.directive';
-import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
 import { RFPOverallController } from './rfpoverall/rfpoverall.controller';
 import { RFPCustomerController } from './rfpcustomer/rfpcustomer.controller';
 import { RFPLoginControler } from './rfplogin/rfplogin.controller';
@@ -15,19 +11,13 @@ import { RFPOutputController } from './rfpoutputdetails/rfpoutputdetails.control
 import { RFPDetailsController } from './rfpdetails/rfpdetails.controller';
 
 
-angular.module('mllRfpClient', ['ui.router', 'toastr', 'ngCsv', 'ngFileUpload'])
-  .constant('malarkey', malarkey)
-  .constant('moment', moment)
+angular.module('mllRfpClient', ['ui.router', 'ngFileUpload'])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
-  .service('githubContributor', GithubContributorService)
-  .service('webDevTec', WebDevTecService)
   .controller('RFPOverallController', RFPOverallController)
   .controller('RFPCustomerController', RFPCustomerController)
   .controller('RFPLoginControler', RFPLoginControler)
   .controller('RFPDashboardController', RFPDashboardController)
   .controller('RFPOutputController', RFPOutputController)
-  .directive('acmeNavbar', NavbarDirective)
-  .directive('acmeMalarkey', MalarkeyDirective)
   .controller('RFPDetailsController', RFPDetailsController);
