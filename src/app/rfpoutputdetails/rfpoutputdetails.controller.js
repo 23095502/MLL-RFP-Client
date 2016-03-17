79f49e603d10f7995d30895de12349ae6baf9bfc
Satyajeet K. Thakare <satyajeetthakre@gmail.com>
2016-03-17 16:13:20 +0530

    * Modal PopUp changes. * Approval for submit functionality added. * Screen 6 UI development.

--- src/app/rfpoutputdetails/rfpoutputdetails.controller.js
+++ src/app/rfpoutputdetails/rfpoutputdetails.controller.js
@@ -2,15 +2,12 @@ export class RFPOutputController {
   constructor($http, $stateParams) {
     'ngInject';
 
-    //$http.get(`http://172.32.0.101/RFPRest/RFPRestService.svc/gettrans/${$stateParams.rfpId}`)
-    $http.get(`http://172.32.0.101/RFPRest/RFPRestService.svc/gettrans/${$stateParams.rfpId}`)
+    $http.get(`http://59.160.18.222/RFPRest/RFPRestService.svc/gettrans/${$stateParams.rfpId}`)
       .then((res) => {
         this.outputdata = res.data;
-        console.log(res.data);
         this.nameoutputdata = this.outputdata[0];
         this.fromLocationOptions = _.uniqBy(this.outputdata, 'FROMLOCATIONNAME');
         this.vehicleTypeOptions = _.uniqBy(this.outputdata, 'VEHICLETYPENAME');
-        console.log(this.outputdata);
       }, (err) => {
         console.error(err);
       });
@@ -45,9 +42,9 @@ export class RFPOutputController {
     }
 
     this.urlMaps = {
-      'contract': 'http://172.32.0.101/bacontract/Service.svc/getproximitybadata',
-      'backhaul': 'http://172.32.0.101/bacontract/Service.svc/dvprdata',
-      'rfphistory': 'http://172.32.0.101/RFPTool/RFPRestService.svc/rfphistory',
+      'contract': 'http://59.160.18.222/bacontract/Service.svc/getproximitybadata',
+      'backhaul': 'http://59.160.18.222/bacontract/Service.svc/dvprdata',
+      'rfphistory': 'http://59.160.18.222/RFPTool/RFPRestService.svc/rfphistory',
       'cleansheet': ''
     }
 
@@ -63,27 +60,12 @@ export class RFPOutputController {
     return _.map(this.outputdata, (v) => (_.pick(v, ['CUSTOMERNAME', 'CASHACCOUNTID', 'FROMLOCATIONNAME'])));
   }
 
-
-  showModal(colname, table) {
+  showModal(colname, table, clickedColName) {
 
     this.selectedLane = table;
+    this.rowClickedColName = clickedColName;
 
-
-    //this.inproxiparam = angular.copy(inproxiparam);
-/*
-    this.inproxiparam.ORIGIN = ;
-    this.inproxiparam.ORIGINSTATE = table.ORIGINSTATE;
-    this.inproxiparam.DESTINATION = table.SEARCH2;
-    this.inproxiparam.DESTINATIONSTATE = table.DESTINATIONSTATE;
-    this.inproxiparam.VEHICLETYPE = table.VEHICLETYPE;
-    this.inproxiparam.DISTANCE = table.DISTANCE;
-    this.inproxiparam.NOOFTRIPS = table.NOOFTRIPS;
-
-    console.log(this.inproxiparam);
-*/
-
-    //this.inproxiparam.ORIGIN = table.TOLOCATIONNAME;
-/*
+    /*
     this.inproxiparam.ORIGIN = this.filterOption.FROMLOCATIONNAME;
     this.inproxiparam.ORIGINSTATE = this.outputdata[0].FROMSTATE;
     this.inproxiparam.DESTINATION = this.outputdata[0].TOLOCATIONNAME;
@@ -91,8 +73,8 @@ export class RFPOutputController {
     this.inproxiparam.VEHICLETYPE = this.filterOption.VEHICLETYPENAME;
     this.inproxiparam.DISTANCE = this.outputdata[0].DISTANCE;
     this.inproxiparam.NOOFTRIPS = this.outputdata[0].NOOFTRIPS;
-*/
     console.log(this.inproxiparam);
+    */
 
     var newfilterRoutes = '{"inproxiparam":' + JSON.stringify(this.inproxiparam) + '}';
 
@@ -101,7 +83,7 @@ export class RFPOutputController {
 
     var req = {
       method: 'POST',
-      url: 'http://172.32.0.101/bacontract/Service.svc/getproximitybadata',
+      url: 'http://59.160.18.222/bacontract/Service.svc/getproximitybadata',
       //url: this.urlMaps[colname],
       headers: {
         'Content-Type': 'application/json'
@@ -112,10 +94,10 @@ export class RFPOutputController {
 
     this.$http(req).then((res) => {
     this.gridData = res.data.getproximitybadataResult;
-    console.log(this.gridData);
     }, (err) => {
       console.error(err);
     });
+
 
     $('#myModalOutputDetails').modal();
 
@@ -125,21 +107,16 @@ export class RFPOutputController {
 
     var revisedOutput = _.chain(this.outputdata).map((output) => {
 
+      output.MODE = 'APIRATE';
       output.CREATEDBY = 1;
       output.CREATEDON = '2016-03-01';
-
       delete output.$$hashKey;
       delete output.ADDRESS;
       delete output.AVERAGELOAD;
       delete output.BACKHAUL;
-      delete output.BACKHAULAVL;
-      delete output.BACKHAULPERCENT;
-      delete output.BAQUOTE;
       delete output.CASHACCOUNTID;
-      delete output.CLEANSHEETRATE;
       delete output.CONTACTNO;
       delete output.CONTACTPERSON;
-      delete output.CONTRACTRATE;
       delete output.CUSTOMERNAME;
       delete output.CUSTTARGETRATE;
       delete output.DISTANCE;
@@ -150,53 +127,30 @@ export class RFPOutputController {
       delete output.ISLOADUNLOADCHARG;
       delete output.ISROUNDTRIP;
       delete output.LOADINGUNLOADINGTIME;
-      delete output.MARKETRATE;
       delete output.MHEREQUIREMENT;
       delete output.NOOFTRIPS;
       delete output.OTHERREQUIREMENT;
       delete output.PACKAGETYPEID;
       delete output.PACKAGETYPENAME;
       delete output.PACKDIMENSION;
-      delete output.PVSRFPRATE;
       delete output.RESULT;
       delete output.RFPDURATION;
       delete output.RFPVOLUME;
-      delete output.SERVICETYPE;
-      delete output.SHIPXRATE;
       delete output.STACKINGNORMS;
       delete output.TOLOCATIONNAME;
       delete output.TOSTATE;
       delete output.TOSTATECODE;
       delete output.TOTALSPEND;
       delete output.VEHICLETYPENAME;
-
-    /*
-    {
-      "RFPID"
-      "BANAME"
-      "APPROVEDAMOUNT"
-      "FROMLOCATIONID"
-      "TOLOCATIONID"
-      "FROMSTATEID"
-      "TOSTATEID"
-      "VEHICLETYPEID"
-      "ACTIVE"
-      "CREATEDBY"
-      "CREATEDON"
-    }
-    */
-
-
       return output;
+
     }).value();
 
-    /*
     var newRFPOutputDetails = '{"apptrans":' + JSON.stringify(revisedOutput) + '}';
 
     var req = {
       method: 'POST',
-      url: 'http://172.32.0.101/RFPRest/RFPRestService.svc/apptrans',
-      //url: this.urlMaps[colname],
+      url: 'http://59.160.18.222/RFPRest/RFPRestService.svc/apptrans',
       headers: {
         'Content-Type': 'application/json'
       },
@@ -206,18 +160,18 @@ export class RFPOutputController {
 
     this.$http(req).then((res) => {
     console.log(res.data);
-    console.log(this.gridData);
+    //console.log(this.gridData);
     }, (err) => {
       console.error(err);
     });
-    */
 
   }
+
 
   updateContractRate(popupGridData){
 
     this.CONTRACTRATE = popupGridData.FREIGHTRATE;
-    this.selectedLane.CONTRACTRATE = popupGridData.FREIGHTRATE;
+    this.selectedLane[this.rowClickedColName] = popupGridData.FREIGHTRATE;
 
   }
 
