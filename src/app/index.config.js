3314610fb032fe63e026f48c97a58fb82b9f5ae5
yoginimokashi23 <mokashi.yogini@mahindra.com>
2016-03-29 18:50:30 +0530

    added new page as market rate

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -2,7 +2,7 @@ export function config ($logProvider, $httpProvider, toasterConfig) {
   'ngInject';
 
   $logProvider.debugEnabled(true);
-  toasterConfig.timeout = 2000;
+  toasterConfig.timeout = 8000;
   //toasterConfig.error =
 
   // http://stackoverflow.com/questions/33660712/angularjs-post-fails-response-for-preflight-has-invalid-http-status-code-404
--- src/app/marketrate/marketrate.controller.js
+++ src/app/marketrate/marketrate.controller.js
@@ -1,17 +1,15 @@
-export class LanesController {
-  constructor($state, $stateParams, $timeout, Upload, masterService, apiService, toaster) {
+export class MarketRateController {
+  constructor($state, $stateParams, $timeout, Upload, masterService, apiService, toaster, $filter) {
     'ngInject';
 
     this.routes = [];
-    this.routes = [];
     this.$timeout = $timeout;
     this.$stateParams = $stateParams;
-    this.isServiceTypeODC;
     this.statename_option = [];
-    this.isSERVICETYPE_option = [];
     this._api = apiService;
     this.$state = $state;
     this.toaster = toaster;
+    this.$filter = $filter;
 
     this.locationname_option = [];
     this.vehicletype = {
@@ -23,14 +21,6 @@ export class LanesController {
       "LOCATIONNAME": null
     };
 
-    this.iswarehousing = $stateParams.iswarehousing;
-    (this.iswarehousing === 'Y') ? this.iswarehousing = true: this.iswarehousing = false;
-
-    //===========================
-    //Get all RFP routes by RFP ID
-    this.getRPFRoutes();
-    //===========================
-
     this.state = {
       "STATEID": null,
       "STATENAME": null,
@@ -40,32 +30,16 @@ export class LanesController {
     this.locationname_option = masterService.getLocations();
     this.statename_option = masterService.getStates();
     this.vehicletypename_option = masterService.getVehicleTypes();
-    this.isPACKAGETYPEID_option = this.prepareForDropdown(['', 'Pallet', 'Corrugated Boxes', 'Bags', 'Trolley', 'Loose']);
-    this.isSERVICETYPE_option = this.prepareForDropdown(['FTL', 'ODC', 'Surface Exp', 'PTL Conventional', 'Fixed Vehicle', 'Air Express']);
 
-    document.getElementsByClassName('tbody-div4')[0].addEventListener('scroll', function(e) {
-      document.querySelector('.tbody-div3 table').style.top = `-${e.target.scrollTop}px`;
-      document.querySelector('.thead-div2 table').style.left = `-${e.target.scrollLeft}px`;
-    });
+      // document.getElementsByClassName('tbody-div4')[0].addEventListener('scroll', function(e) {
+      // document.querySelector('.tbody-div3 table').style.top = `-${e.target.scrollTop}px`;
+      // document.querySelector('.thead-div2 table').style.left = `-${e.target.scrollLeft}px`;
+    // });
 
-    this.resetRoute();
+    this.add();
   }
 
-  getRPFRoutes() {
-    this._api.get(`getrfproutebyid/${this.$stateParams.rfpid}`).then((res) => {
-      this.routes = res.data;
-      this.$timeout(this.adjustScrollableTable);
-    }, (err) => {
-      //console.error(err);
-      this.toaster.error(`${err.status} : ${err.statusText}`);
-    });
-  }
-
-  init() {
-
-  }
-
-  adjustScrollableTable() {
+  /*adjustScrollableTable() {
 
     var $div1HeadAll = document.querySelectorAll('.thead-div1 th');
     var $div2HeadAll = document.querySelectorAll('.thead-div2 th');
@@ -125,206 +99,20 @@ export class LanesController {
     } else {
       this.isServiceTypeODC = false;
     }
-  }
-
-  resetRoute() {
-    this.route = {
-      "ACTIVE": 'Y',
-      "APPROVEDAMOUNT": 0,
-      "AVERAGELOAD": 0,
-      "BACKHAUL": 0,
-      "CUSTTARGETRATE": 0,
-      "DISTANCE": 0,
-      "FREQUENCY": 0,
-      "FROMLOCATION": 0,
-      "FROMLOCATIONNAME": '',
-      "FROMSTATE": '',
-      "FROMSTATEID": 0,
-      "ISLOADUNLOADCHARG": 'N',
-      "ISROUNDTRIP": 'N',
-      "LOADINGUNLOADINGTIME": 0,
-      "LOCATIONNAME": '',
-      "MHEREQUIREMENT": 'NA',
-      "NOOFTRIPS": 0,
-      "OTHERREQUIREMENT": 'NA',
-      "PACKAGETYPEID": 1,
-      "PACKDIMENSION": 'NA',
-      "RFPDURATION": 0,
-      "RFPID": this.$stateParams.rfpid,
-      "RFPVOLUME": 0,
-      "SERVICETYPE": this.isSERVICETYPE_option[0].val,
-      "STACKINGNORMS": '',
-      "TOLOCATION": 0,
-      "TOLOCATIONNAME": '',
-      "TOSTATE": '',
-      "TOSTATEID": 0,
-      "VEHICLETYPEID": 0,
-      "VEHICLETYPENAME": '',
-      "MODE": 'INSERT',
-      "SEARCH1": '',
-      "SEARCH2": '',
-      "SEARCH3": '',
-      "DIRTY": false
-    }
-  }
+  }*/
 
   add() {
-    this.route.DIRTY = true;
-    this.route.ACTIVE = 'A';
-    this.route.APPROVEDAMOUNT = 0;
-    this.route.AVERAGELOAD = 'NA';
-    this.route.BACKHAUL = 0;
-    this.route.DISTANCE = 0;
-    this.route.FREQUENCY = 'NA';
-    this.route.ISLOADUNLOADCHARG = 'N';
-    this.route.LOCATIONNAME = '';
-    this.route.MHEREQUIREMENT = 'NA';
-    this.route.PACKDIMENSION = 'NA';
-    this.route.RFPID = this.$stateParams.rfpid;
-    this.route.RFPVOLUME = 0;
-    this.route.SEARCH1 = '';
-    this.route.SEARCH2 = '';
-    this.route.SEARCH3 = '';
-    this.route.CREATEDBY = 1;
-    this.route.CREATEDON = '2016-03-01';
-
-    this.routes.push(angular.copy(this.route));
-    this.resetRoute();
-    this.editingIndex = null;
-
-    this.toaster.success('Lane saved successfully');
-    //---------------------
-    //set width to route grid columns
-    this.$timeout(this.adjustScrollableTable);
-    //---------------------
-    $('#myModal').modal('hide');
-  }
-
-  edit(route, index) {
-    this.route = angular.copy(route);
-    this.route.DIRTY = true;
-    this.route.SEARCH1 = '';
-    this.route.SEARCH2 = '';
-    this.route.SEARCH3 = '';
-    this.route.MODE = 'INSERT';
-    this.route.CREATEDBY = 1;
-    this.route.CREATEDON = '2016-03-01';
-    this.editingIndex = index;
-    $('#myModal').modal();
-    //this.toaster.success('Lane updated successfully');
-  }
-
-  save() {
-    this.routes[this.editingIndex] = this.route;
-    this.editingIndex = null;
-    this.toaster.success('Lane updated successfully');
-    //---------------------
-    //set width to route grid columns
-    this.$timeout(this.adjustScrollableTable);
-    //---------------------
-    $('#myModal').modal('hide');
-  }
-
-  delete() {
-
-    this.route.MODE = 'DELETE';
-    this.routes[this.editingIndex] = this.route;
-    this.editingIndex = null;
-    this.toaster.success('Lane deleted successfully');
-    //---------------------
-    //set width to route grid columns
-    this.$timeout(this.adjustScrollableTable);
-    //---------------------
-    $('#myModal').modal('hide');
-  }
-
-  submitData() {
-
-    var filterRoutes = _.chain(this.routes).filter((o) => {
-      return o.DIRTY;
-    }).map((route) => {
-      delete route.$$hashKey;
-      delete route.DIRTY;
-      delete route.FROMLOCATIONNAME;
-      delete route.LOCATIONNAME;
-      delete route.PACKAGETYPENAME;
-      delete route.RESULT;
-      delete route.TOLOCATIONNAME;
-      delete route.FROMSTATE;
-      delete route.TOSTATE;
-      delete route.VEHICLETYPENAME;
-      return route;
-    }).value();
-
-    var newfilterRoutes = {
-      rfproute: filterRoutes
-    };
-
-
-    this._api.post('routeupdate', newfilterRoutes).then((r) => {
-      this._api.get(`apiupdate/${this.$stateParams.rfpid}`).then((res) => {
-        this.$state.go('dashboard');
-      }, (err) => {
-        console.error(err);
-      });
-
-    }, (e) => {
-      console.error(e);
-    });
-
-    this.toaster.success('Lanes saved successfully');
-  }
-
-  uploadBlobOrFile(blobOrFile) {
-
-    var client = new XMLHttpRequest();
-    client.open('POST', `http://115.113.135.239/RFPRoute/RFPImportRoute.svc/rfprouteupload/${this.$stateParams.rfpid}/Routeupload/1`, false);
-    //client.open('POST', `http://localhost:52202/RFPImport/Service.svc/Upload/RFPUpload/${this.$stateParams.rfpid}`, false);
-    client.setRequestHeader("Content-Type", "multipart/form-data");
-
-    /* Check the response status */
-    client.onreadystatechange = () => {
-      //console.log("rdystate: " + client.readyState + " status: " + client.status + " Text: " + client.statusText);
-      if (client.readyState == 4 && client.status == 200) {
-
-
-        var response = JSON.parse(client.responseText);
-        console.log(`FilePath: ${response.FilePath}`);
-        //console.log(`FileLength: ${response.FileLength}`);
-        console.log(`FileName: ${response.FileName}`);
-
-        if (response.ErrorMessage != '') {
-          console.log(`ErrorMessage: ${response.ErrorMessage}`);
-        } else {
-          console.log(`SuccessMessage: ${response.SuccessMessage}`);
-        }
-        console.log(`NoOfRecordsUpdated: ${response.NoOfRecordsUpdated}`);
-        console.log(response.ErrorData);
-
-        this.gridData = response.ErrorData;
-
-        if (this.gridData != null) {
-          $('#myModalErrorList').modal();
-        }
-        //===========================
-        //Get all RFP routes by RFP ID
-        this.getRPFRoutes();
-        //===========================
-        this._api.get(`apiupdate/${this.$stateParams.rfpid}`).then((res) => {
-          //this.$state.go('dashboard');
-        }, (err) => {
-          //console.error(err);
-          this.toaster.error(`${err.status} : ${err.statusText}`);
-        });
-        //===========================
-        $('#myModalBrowse').modal('hide');
-      }
+    this.marketrate = {
+        "OriginCity": '',
+        "OriginState": '',
+        "DestinationCity": '',
+        "DestinationState": '',
+        "vehicletype": '',
+        "Rate": 0,
+        "Remarks" : '',
+        "CREATEDBY": 1
     }
 
-    /* Send to server */
-    client.send(blobOrFile);
-
-    this.toaster.success("Data imported");
   }
 
   map(id, list, idMatcher, nameKey) {
@@ -335,16 +123,31 @@ export class LanesController {
     }
   }
 
-  showModal() {
+// for submit data ...
+  addMarketRate() {
+    var marketRateData = {
+      "marketrate": {
+        "OriginCity": this.marketrate.OriginCity,
+        "OriginState": this.marketrate.OriginState,
+        "DestinationCity": this.marketrate.DestinationCity,
+        "DestinationState": this.marketrate.DestinationState,
+        "VehicleType": this.marketrate.VehicleType,
+        "Rate": this.marketrate.Rate,
+        "Remarks" : '',
+        "CreatedBy": 1
+      }
+    };
 
-    this.resetRoute();
-    this.editingIndex = null;
-    $('#myModal').modal();
+    //console.log('Hi');
+    console.log(marketRateData);
 
+    this._api.post('marketrate', marketRateData).then((response) => {
+      // console.log(response);
+      this.toaster.success('Market Rate saved successfully');
+      this.add();
+    }, (error) => {
+      console.log(error);
+      // this.toaster.error(`${error.status} : ${error.statusText}`);
+    });
   }
-
-  closeModal() {
-    $('#myModalErrorList').modal('hide');
-  }
-
 }
